98e3144a5ae0f1e1188c5ae250f01f880bba1a8f
Megatron419 <135339210+Megatron419@users.noreply.github.com>
2026-05-20 12:42:35 +0200

    Added solution to Exercise: 2.3.1 - Key Establishment

---
+++ syssec.py
@@ -0,0 +1,67 @@
+import os.path as path
+import json
+import socket
+
+class Module:
+    def __init__(self, incoming=False, verbose=False, options=None):
+        # extract the file name from __file__. __file__ is proxymodules/name.py
+        self.name = path.splitext(path.basename(__file__))[0]
+        self.description = 'Simply print the received data as text'
+        self.incoming = incoming  # incoming means module is on -im chain
+        self.find = None  # if find is not None, this text will be highlighted
+
+    def execute(self, data):
+        print(f"Incoming data: {data}")
+
+        ### Work with data here ###
+        # data_json = json.loads(data)
+        # data_json["content"] = "Blablabla"
+        # data = json.dumps(data_json)
+        # print(f"Outgoing data: {data}")
+        # return data + "\n"
+
+        #Using Reflection Attack as mentioned in the lecture
+        #Impersonating Bob and sending oblivious Alice her own replies
+
+        data_json = json.loads(data)
+
+        if data_json["id"] == 1:
+            original_sender, original_receiver = data_json["sender"], data_json["receiver"]
+            data_json["sender"], data_json["receiver"] = original_receiver, original_sender
+
+            nonce = str(data_json["content"])
+
+            #How to get last part of string
+            #https://stackoverflow.com/questions/16118379/get-characters-of-a-string-from-right
+
+            data_json["content"] = "Bob," + nonce[-10:]
+            data = json.dumps(data_json) + '\n'
+
+        if data_json["id"] == 2:
+            original_sender, original_receiver = data_json["sender"], data_json["receiver"]
+            data_json["sender"], data_json["receiver"] = original_receiver, original_sender
+            data = json.dumps(data_json) + '\n'
+
+        if data_json["id"] == 3:
+            original_sender, original_receiver = data_json["sender"], data_json["receiver"]
+            data_json["sender"], data_json["receiver"] = original_receiver, original_sender
+            data = json.dumps(data_json) + '\n'
+
+        if data_json["id"] == 4:
+            original_sender, original_receiver = data_json["sender"], data_json["receiver"]
+            data_json["sender"], data_json["receiver"] = original_receiver, original_sender
+            data_json["content"] = "1234567890" #Making our own nonce which does not matter, and establishing connection with Alice
+            data = json.dumps(data_json) + '\n'
+
+            # After mirroring last message and sending Alice her own non encrypted nonce, getting flag: n3v3r_tru5t_b0b
+
+
+        print(f"Outgoing data: {data}")
+        return data
+
+
+
+
+
+if __name__ == '__main__':
+    print('This module is not supposed to be executed alone!')
